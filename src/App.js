f1d8856971e8bbcb473018b3413c87f155c857b7
mohammad issa <jesus@forbes-dev-004.local>
2019-05-19 10:35:06 -0400

    add info page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Home } from './pages/home/home'
 import { Products } from './pages/products/products'
+import { Info } from './pages/info/info'
 import { NotFound } from './pages/404/404'
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/products/" exact component={Products} />
+          <Route path="/info/" exact component={Info} />
           <Route component={NotFound} />
         </Switch>
       </Router>
---
+++ src/pages/info/info.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './info.css';
+
+export class Info extends Component {
+  render() {
+    return (
+      <div className='info-warpper'>
+        <h1>welcome To Info Page</h1>
+      </div>
+    );
+  }
+}
