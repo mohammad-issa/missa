2933123e896577c313f1baf3da04c095bc430310
mohammad issa <jesus@forbes-dev-004.local>
2019-05-19 09:24:20 -0400

    init products

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
+import './ProductCard.css';
+
 export class ProductCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.shareWithFB= this.shareWithFB.bind(this);
+    this.addToCart= this.addToCart.bind(this);
+  }
+
+  shareWithFB() {
+    alert('share with facebook');
+  }
+
+  addToCart() {
+    alert('add to cart');
+  }
+
   render() {
     return (
-      <h1>ProductCard</h1>
+      <div className='product-card'>
+        <p>{this.props.item.name}</p>
+        <div>
+          <button onClick={this.shareWithFB}>Facebook</button>
+          <button onClick={this.addToCart}>Add to cart</button>
+        </div>
+      </div>
     );
   }
 }
--- src/pages/products/products.js
+++ src/pages/products/products.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { ProductCard } from '../../components/ProductCard/ProductCard';
+
+import './products.css';
 
 export class Products extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [
+        {
+          name: 'Product 1'
+        }, {
+          name: 'Product 2'
+        }
+      ]
+    }
+
+    this.renderCards = this.renderCards.bind(this);
+  }
+
+  renderCards() {
+    return this.state.products.map((item, index) => {
+      return <ProductCard key={index} item={item}/>
+    })
+  }
+
   render() {
     return (
-      <h1>Products</h1>
+      <div className='products-warpper'>
+        <h1>Products</h1>
+        <hr/>
+        <div className='products-list'>
+          {this.renderCards()}
+        </div>
+      </div>
     );
   }
 }
